769bba491e1490123d6f8e26c29c69920b63b1f9
mariohernandez <mrhernandez@mednet.ucla.edu>
2024-08-31 12:32:31 -0700

    Updates alert and button styles

--- src/components/01-atoms/alert/alert.stories.jsx
+++ src/components/01-atoms/alert/alert.stories.jsx
@@ -6,50 +6,10 @@ const component = {
   title: 'Atoms/Alert',
 };
 
-export const AlertInfo = {
-  name: 'Info alert',
+export const AlertDefault = {
+  name: 'Alert',
   render: (args) => parse(alert(args)),
   args: { ...data },
-};
-
-export const AlertScheduled = {
-  ...AlertInfo,
-  name: 'Scheduled alert',
-  render: (args) => parse(alert(args)),
-  args: {
-    ...data,
-    type: 'scheduled',
-  },
-};
-
-export const AlertMaintenance = {
-  ...AlertInfo,
-  name: 'Maintenance alert',
-  render: (args) => parse(alert(args)),
-  args: {
-    ...data,
-    type: 'maintenance',
-  },
-};
-
-export const AlertOutage = {
-  ...AlertInfo,
-  name: 'Outage alert',
-  render: (args) => parse(alert(args)),
-  args: {
-    ...data,
-    type: 'outage'
-  },
-};
-
-export const AlertEmergency = {
-  ...AlertInfo,
-  name: 'Outage alert',
-  render: (args) => parse(alert(args)),
-  args: {
-    ...data,
-    type: 'emergency'
-  },
 };
 
 export default component;
