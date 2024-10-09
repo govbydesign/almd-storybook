bed138283f94ea89f28fff66b0dd763de333f9a0
Eduardo Noyer <eduardo@octahedroid.com>
2024-10-09 14:57:55 -0600

    Remove storybook content, keep workflow and README

--- .storybook/almd.js
+++
@@ -1,9 +0,0 @@
-import { create } from '@storybook/theming/create';
-
-export default create({
-  base: 'dark',
-  brandTitle: 'Alabama Middle District',
-  brandUrl: 'https://www.almd.uscourts.gov',
-  brandImage: '',
-  brandTarget: '_blank',
-});
